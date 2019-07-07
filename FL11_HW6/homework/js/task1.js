a2c1306d909fc789d930f735379b82927611d420
Roman Saldan <35783096+romasaldan@users.noreply.github.com>
2019-07-07 16:26:26 +0300

    update

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -22,8 +22,11 @@ let c2 = +prompt('enter c2',0);
 if (Number.isNaN(c2)) {
 	console.log('you have not entered numeric values for c2. Refresh the page and start again.');
 }
-const numberOfPoint = 2;
-if ((a1+b1)/numberOfPoint===c1&&(a2+b2)/numberOfPoint===c2) {
+const PROBLEM_FLOAT_NUMBERS = 1000000; //this number choosen because I think it is enough big to solve problem with summing two float numbers
+const AMOUNT_OF_POINT = 2;
+let firstCondition = (a1*PROBLEM_FLOAT_NUMBERS+b1*PROBLEM_FLOAT_NUMBERS)/(AMOUNT_OF_POINT*PROBLEM_FLOAT_NUMBERS)===c1;
+let secondCondition = (a2*PROBLEM_FLOAT_NUMBERS+b2*PROBLEM_FLOAT_NUMBERS)/(AMOUNT_OF_POINT*PROBLEM_FLOAT_NUMBERS)===c2
+if (firstCondition&&secondCondition) {
 	console.log(true);
 } else {
 	console.log(false);
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,21 +1,20 @@
 let a = +prompt('enter a',0);
-if (Number.isNaN(a)) {
+if (isNaN(a)) {
 	console.log('You have not entered number for a');
 } else if (a<0) {
-	console.log('length of side a must be positive');
+	console.log('length of side a have to be positive');
 }
 let b = +prompt('enter b',0); 
-if (Number.isNaN(b)) {
+if (isNaN(b)) {
 	console.log('You have not entered number for b')
 } else if (b<0) {
-	console.log('length of side b must be positive');
+	console.log('length of side b have to be positive');
 }
-
 let c = +prompt('enter c',0); 
-if (Number.isNaN(c)) {
+if (isNaN(c)) {
 	console.log('You have not entered number for c')
 } else if (c<0) {
-	console.log('length of side c must be positive');
+	console.log('length of side c have to be positive');
 }
 if (a>0&&a>0&&c>0&&a+b>c&&a+c>b&&c+b>a) {
 	let amountOfEqualSides = 0;
@@ -31,18 +30,13 @@ if (a>0&&a>0&&c>0&&a+b>c&&a+c>b&&c+b>a) {
 	const AMOUNT_OF_EQUAL_SIDES_IN_NORMAL_TRIANGLE = 0; 
 	const AMOUNT_OF_EQUAL_SIDES_IN_ISOSCELES_TRIANGLE = 1;  
 	const AMOUNT_OF_EQUAL_SIDES_IN_EQUIVALENT_TRIANGLE = 3;  
-	switch (amountOfEqualSides) {
-		case AMOUNT_OF_EQUAL_SIDES_IN_NORMAL_TRIANGLE:
-			console.log('Normal triangle');
-			break;
-		case AMOUNT_OF_EQUAL_SIDES_IN_ISOSCELES_TRIANGLE:	
-			console.log('Isosceles triangle');
-			break;
-		case AMOUNT_OF_EQUAL_SIDES_IN_EQUIVALENT_TRIANGLE: 
-			console.log('Eequivalent triangle');
-			break;
-		default:	
-	}	
+	if (amountOfEqualSides===AMOUNT_OF_EQUAL_SIDES_IN_NORMAL_TRIANGLE) {
+		console.log('Normal triangle');
+	} else if (amountOfEqualSides===AMOUNT_OF_EQUAL_SIDES_IN_ISOSCELES_TRIANGLE) {
+		console.log('Isosceles triangle');
+	} else if(amountOfEqualSides===AMOUNT_OF_EQUAL_SIDES_IN_EQUIVALENT_TRIANGLE) {
+		console.log('Eequivalent triangle');
+	}
 } else {
 	console.log('Triangle doesn’t exist');
 }
