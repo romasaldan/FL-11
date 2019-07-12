735da7a43a953a083e75a72ee8f31e54f10c01fb
Roman Saldan <35783096+romasaldan@users.noreply.github.com>
2019-07-12 14:18:52 +0300

    Update index.js

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,4 +1,4 @@
-//1
+//0
 function getNumbers(string) {
 	let arrayOfNumbers = [];
 	let counterOfNumbers = 0;
@@ -13,7 +13,6 @@ function getNumbers(string) {
 }
 console.log(getNumbers('n1um3ber95'));
 //1
-//2
 function findTypes() {
 	let counterOfTypes = {};
 	for (let i = 0;i<arguments.length;i++) {
@@ -26,4 +25,36 @@ function findTypes() {
 	}
 	return counterOfTypes;
 }
-console.log(findTypes(null,1,'hello',1,'5')); 
+console.log(findTypes(null,1,'hello',1,'5',true)); 
+//2
+function executeforEach(array,functionForExecuting) {
+	for (let i = 0;i<array.length;i++) {
+		functionForExecuting(array[i]);	
+	}
+}
+executeforEach([0,1,1], function(el) {
+	console.log(el) 
+}); 
+//3
+function mapArray(array,functionForExecuting) {
+	let  newArray = []; 
+	executeforEach(array,function(el) {
+		newArray.push(functionForExecuting(el))
+	})
+	return newArray;
+}
+console.log(mapArray([0, 1, 1], function(el) {
+	return el + 3; 
+})) 
+//4
+function filterArray(array,functionForExecuting) {
+	let  newArray = [];
+	
+	executeforEach(array,function(el) {
+		newArray.push(functionForExecuting(el))
+	})
+	return newArray;	
+}
+console.log(filterArray([2, 5, 8], function(el) { 
+	return el > 3; 
+}))
