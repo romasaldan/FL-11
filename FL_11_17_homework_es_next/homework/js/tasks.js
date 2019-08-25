9c6b5c9667e286071e7f826aecd127124b2593cc
Roman Saldan <35783096+romasaldan@users.noreply.github.com>
2019-08-25 10:31:20 +0300

    Update tasks.js

--- FL_11_17_homework_es_next/homework/js/tasks.js
+++ FL_11_17_homework_es_next/homework/js/tasks.js
@@ -1,41 +1,58 @@
 //№1
 function maxElement(...rest) {
-	return Math.max.apply(null,rest[0])
+	if (rest[0] instanceof Array) {
+		return Math.max.apply(null,rest[0])
+	} else {
+		throw Error('parameter should be array')
+	}	
 }
 console.log(maxElement([1,4,3,2,5]))
 //№2
 function copyArray(array) {
-	return Array.from(array);
+	return [...array];
 }
 const arr = [2,3,4]
 console.log(copyArray(arr) === arr)
-
-const s = Symbol('ff')
-const s1 = Symbol('ff')
-const huj = {
-	v:3
-}
 //№3
-let addUniqueId = (function() {
+const objWithoutID = {
+	somePropetties:3
+}
+let addUniqueId = (()=> {
 	let counter = 0;
 	return function (object) {
 		const newObj = {}
 		Object.assign(newObj,object)
-		let id = Symbol('id')
-		newObj[id] = counter;
+		newObj[Symbol('id')] = counter;
 		counter++
 		return newObj;
 	}
 })()
-const huj1 = addUniqueId(huj)
-console.log(huj1)
-console.log(huj)
+const objWithID = addUniqueId(objWithoutID)
+console.log(objWithID)
+console.log(objWithoutID)
 //№4
-
-
-
-
-
+const oldObject = {
+	name: 'Someone',
+	details: {
+		id: 1,
+		age: 11,
+		university: 'UNI'
+	}
+}
+function reGroupObject(obj) {
+	let {name} = obj
+	let {id,age,university} = obj.details;
+	const newObj = {
+		university,
+		user: {
+			age,
+			firstName: name,
+			id
+		}
+	}
+	return newObj
+}
+console.log(reGroupObject(oldObject))
 //№5
 function findUniqueElements(array) {
 	let set = new Set(array)
@@ -53,3 +70,41 @@ function add(a=(()=>{throw Error('Missing property as required')})(),b=(()=>{thr
 	return a+b
 }
 console.log(add(2,3))
+//№8
+function getNames() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(data => data.json())
+    .then(array => {
+        const arrayOfNames = [];
+        array.forEach(element => {
+            arrayOfNames.push(element.name)
+        });
+        arrayOfNames.sort()
+        arrayOfNames.forEach(element => {
+            console.log(element)
+        });
+    })
+    .catch(error=>console.log(error))
+}
+getNames()
+
+// №9
+async function rewrittenGetNames () {
+    try {
+        const users = (async ()=>{
+            const request = await fetch('https://jsonplaceholder.typicode.com/users')
+            const data = await request.json()
+            const arrayOfNames = [];
+            data.forEach(element => {
+                arrayOfNames.push(element.name)
+            });
+            arrayOfNames.sort()
+            arrayOfNames.forEach(element => {
+                console.log(element)
+            });
+            })()            
+    } catch (error) {
+        console.log(error)
+    }
+}  
+rewrittenGetNames()
